7f73c18fadcfeb3fc543008ba653fa19573be529
Rajeshwory Shrestha <shrestharajeshwory@gmail.com>
2025-01-28 13:16:11 -0500

    feat: add pagination component to ui

---
+++ src/components/pagination-footer.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import {
+    Pagination,
+    PaginationContent,
+    PaginationEllipsis,
+    PaginationItem,
+    PaginationLink,
+    PaginationNext,
+    PaginationPrevious,
+  } from "@/components/ui/pagination"
+
+export const PaginationComponent = ({currentPage, totalPages, onPageChange}) => {
+    return(
+        <Pagination>
+      <PaginationContent>
+        <PaginationItem>
+          <PaginationPrevious
+            href="#"
+            onClick={(e) => {
+              e.preventDefault()
+              onPageChange(Math.max(1, currentPage - 1))
+            }}
+          />
+        </PaginationItem>
+        {[...Array(totalPages)].map((_, i) => (
+          <PaginationItem key={i}>
+            <PaginationLink
+              href="#"
+              isActive={currentPage === i + 1}
+              onClick={(e) => {
+                e.preventDefault()
+                onPageChange(i + 1)
+              }}
+            >
+              {i + 1}
+            </PaginationLink>
+          </PaginationItem>
+        ))}
+        <PaginationItem>
+          <PaginationNext
+            href="#"
+            onClick={(e) => {
+              e.preventDefault()
+              onPageChange(Math.min(totalPages, currentPage + 1))
+            }}
+          />
+        </PaginationItem>
+      </PaginationContent>
+    </Pagination>
+    )
+}
