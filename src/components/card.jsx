cc3f6d707fa3d73b5cfc345943daa46f34f941cf
Rajeshwory Shrestha <shrestharajeshwory@gmail.com>
2025-02-04 12:17:04 -0500

    style: add css to the card component

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -2,7 +2,7 @@ import React from "react"
 
 export const Card = ({user}) => {
     return(
-        <div className="flex flex-col border-2 rounded-lg border-slate-200 text-black-500 text-base bg-slate-100 p-2 hover:border-purple-500 hover:bg-white">
+        <div className="flex flex-col border-2 rounded-lg border-slate-200 text-black-500 text-base bg-slate-100 p-5 sm:p-2 hover:border-purple-500 hover:bg-white overflow-x-auto">
             <label>{user.name}</label>
             <label>{user.email}</label>
             <label>{user.phone}</label>
