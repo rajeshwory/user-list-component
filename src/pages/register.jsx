7b82ba2dd9c97d6a9beb9ab7be4b3b3006bf88f5
Rajeshwory Shrestha <shrestharajeshwory@gmail.com>
2025-02-11 11:57:38 -0500

    feat: make input component

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -17,86 +17,97 @@ const validationSchema = z
     })
 
 
+ const LabelInput = ({error, label, placeholder, register, name}) => {
+    return(
+        <>
+            <label>{label}</label>
+                <input
+                    placeholder={placeholder}
+                    className="mt-2 border border-slate-400 rounded-lg p-1 w-auto"
+                    {...register(name)}
+                />
+                {error && <span className="text-sm text-red-500 mb-5">{error?.message}</span>}
+        </>
+    )
+ }   
+
+ const LabelPassword = ({label, placeholder, register, name, error}) => {
+
+    const [isVisible, setIsVisible] = useState(false)
+    const toggleVisibility = () => setIsVisible(prevState => !prevState)
+    return(
+        <>
+          <label >{label}</label>
+                <div className="relative">
+                    <input
+                        type={isVisible ? "text" : "password"}
+                        required
+                        placeholder={placeholder}
+                        className="mt-2 w-auto border border-slate-400 rounded-lg p-1 pr-8"
+                        {...register(name, { required: true })}
+                    />
+                     <span
+                        type="button"
+                        onClick={toggleVisibility}
+                        aria-label={isVisible ? "Hide password" : "Show password"}
+                        aria-pressed={isVisible}
+                        aria-controls="password"
+                        className="absolute right-2 top-1/2 transform -translate-y-1/4 cursor-pointer"
+                     >{isVisible ? (
+                        <EyeOff size={20} aria-hidden="true" />
+                     )
+                        : (
+                            <Eye size={20} aria-hidden="true" />
+                        )}
+                     </span>
+                 </div>
+                {error && <span className="text-sm text-red-500 mb-5">{error.message}</span>}
+        </>
+    )
+ }
+
+
 
 export const Register = () => {
-
-    const [isPasswordVisible, setIsPasswordVisible] = useState(false)
-    const [isConfirmVisible, setIsConfirmVisible] = useState(false)
-
-    const togglePasswordVisibility = () => setIsPasswordVisible(prevState => !prevState)
-    const toggleConfirmVisibility = () => setIsConfirmVisible(prevState => !prevState)
 
     const { register, handleSubmit, formState: { errors, isValid } } = useForm({ resolver: zodResolver(validationSchema), mode: "onChange" })
     const submitForm = (data) => {
         console.log(data)
     }
+
     return (
-        <div>
+        <div className="flex flex-col border border-slate-300 items-center">
             <form onSubmit={handleSubmit(submitForm)} className="flex flex-col">
-                <label htmlFor="">Name</label>
-                <input
-                    required
-                    placeholder="Enter your name"
-                    className="mt-2 border border-slate-400 rounded-lg p-1 max-w-72"
-                    {...register('name')}
+                <h2 className="text-3xl text-center">Sign up</h2>
+                
+                <LabelInput 
+                label="Name" 
+                placeholder="Enter your name"
+                error={errors.name}
+                name="name"
+                register={register}
                 />
-                {errors.name && <span className="text-sm text-red-500 mb-5">{errors.name.message}</span>}
-                <label htmlFor="">Email</label>
-                <input
-                    required
-                    placeholder="Enter your email"
-                    className="mt-2 border border-slate-400 rounded-lg p-1 max-w-72"
-                    {...register('email')}
+                <LabelInput
+                label="Email"
+                placeholder="Enter your email"
+                error={errors.email}
+                name="email"
+                register={register}
                 />
-                {errors.email && <span className="text-sm text-red-500 mb-5">{errors.email.message}</span>}
-                <label htmlFor="">Password</label>
-                <div className="">
-                    <input
-                        type={isPasswordVisible ? "text" : "password"}
-                        required
-                        placeholder="Enter your password"
-                        className="mt-2 border border-slate-400 rounded-lg p-1 min-w-72"
-                        {...register('password', { required: true })}
-                    />
-                    <button
-                        type="button"
-                        onClick={togglePasswordVisibility}
-                        aria-label={isPasswordVisible ? "Hide password" : "Show password"}
-                        aria-pressed={isPasswordVisible}
-                        aria-controls="password"
-                        className="ml-2 cursor-pointer"
-                    >{isPasswordVisible ? (
-                        <EyeOff size={20} aria-hidden="true" />
-                    )
-                        : (
-                            <Eye size={20} aria-hidden="true" />
-                        )}</button>
-                </div>
-                {errors.password && <span className="text-sm text-red-500 mb-5">{errors.password.message}</span>}
-                <label>Confirm Password</label>
-                <div>
-                    <input
-                        type={isConfirmVisible ? "text" : "password"}
-                        required
-                        placeholder="Confirm password"
-                        className="mt-2 border border-slate-400 rounded-lg p-1 min-w-72"
-                        {...register('confirmPassword', { required: true })}
-                    />
-                    <button
-                        type="button"
-                        onClick={toggleConfirmVisibility}
-                        aria-label={isConfirmVisible ? "Hide password" : "Show password"}
-                        aria-pressed={isConfirmVisible}
-                        aria-controls="confirmPassword"
-                        className="ml-2 cursor-pointer"
-                    >{isConfirmVisible ? (
-                        <EyeOff size={20} aria-hidden="true" />
-                    )
-                        : (
-                            <Eye size={20} aria-hidden="true" />
-                        )}</button>
-                </div>
-                {errors.confirmPassword && <span className="text-sm text-red-500 mb-5">{errors.confirmPassword.message}</span>}
+                <LabelPassword 
+                  label="Password"
+                  placeholder="Enter your password"
+                  register={register}
+                  name="password"
+                  error={errors.password}
+                />
+                <LabelPassword 
+                 label="Confirm Password"
+                 placeholder="Confirm password"
+                 register={register}
+                 name="confirmPassword"
+                 error={errors.confirmPassword}
+                />
                 <button type="submit" disabled={!isValid} className="border border-purple-400 bg-purple-500 text-xl p-1 rounded-lg mt-10 text-white max-w-28 disabled:bg-blue-200 disabled:border-blue-200 disabled:text-gray-500">Register</button>
             </form>
         </div>
