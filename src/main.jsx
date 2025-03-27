ad5496e20a614a436899b0839d5b967b72ff3252
Rajeshwory Shrestha <shrestharajeshwory@gmail.com>
2025-03-27 05:19:38 -0400

    feat: add routes to detail page and dashboard

--- src/main.jsx
+++ src/main.jsx
@@ -5,18 +5,20 @@ import App from './App.jsx'
 import { Register } from './pages/register.jsx'
 import { Login } from './pages/login.jsx'
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
+import UserDetail from './pages/user-detail.jsx'
+import Dashboard from './pages/dashboard.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
     <Routes>
-      <Route path='/' element={<Navigate to="/register"/>}/>
+      <Route path='/' element={<Navigate to="/app"/>}/>
       <Route path='/register' element={<Register />}/>
       <Route path='/login' element={<Login />}/>
       <Route path='/app' element={<App />}/>
+      <Route path='/user' element={<UserDetail />} />
+      <Route path='/dashboard' element={<Dashboard />}/>
     </Routes>
     </BrowserRouter>
-    {/* <Register /> */}
-    {/* <App /> */}
   </StrictMode>,
 )
